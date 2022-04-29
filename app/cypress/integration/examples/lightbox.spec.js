98ab60617436b047619ede3fcda2acae64b88d39
Zetsuy <jeremygarciamobile@gmail.com>
2022-04-29 15:53:35 +0200

    like image

--- app/cypress/integration/examples/lightbox.spec.js
+++ app/cypress/integration/examples/lightbox.spec.js
@@ -10,18 +10,51 @@ describe('caesar app', () => {
     //ouverture lightbox
     it('open lightbox', () =>{
         //open lightbox
-        cy.get('[data-cy=lightbox]').click()
+        cy.dataCy('lightbox').click()
 
     })
 
     //fermeture lightbox
     it('close lightbox', () =>{
         //open lightbox
-        cy.get('[data-cy=lightbox]').click()
+        cy.dataCy('lightbox').click()
 
-        cy.get('[data-cy=background-lightbox]').click(0,0);
+        cy.dataCy('background-lightbox').click(0,0);
 
     })
+
+it('like image', () =>{
+    //open lightbox
+    cy.dataCy('lightbox').click()
+
+    cy.dataCy('lightbox-2').scrollTo('bottom')
+
+    //add like
+    cy.dataCy('bouton-like').click()
+
+    
+    //compteur like
+    cy.dataCy('nombre-like')
+    .should('contain', '1')
+
+    cy.dataCy('background-lightbox').click(0,0);
+
+    cy.dataCy('lightbox').trigger('mouseover');
+
+    cy.dataCy('nombre-like-hover')
+    .should('contain', '1')
+
+    /*
+       .type('1').should('have.value', '1')
+
+       cy.get('[data-cy=texte]')
+       .type('test').should('have.value', 'test')
+
+       cy.get('[data-cy=button-cypher]').click()
+       cy.get('[data-cy=resultat]')
+       .should('contain', 'uftu')
+       */
+})
 
 
 
